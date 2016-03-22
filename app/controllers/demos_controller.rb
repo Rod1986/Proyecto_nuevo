fab383d4c6aa75c48632c23e519a9a9183e864c1
Rodrigo <hein.rodrigo@gmail.com>
2016-03-22 17:32:15 -0300

    database added

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -12,6 +12,25 @@ class DemosController < ApplicationController
     render layout:'landing'
   end
 
+  def show
+    @users = User.all
+  end
+
+  def save_user
+    if params[:name].present? && params[:lastname].present? && params[:age].present?
+       # Puede ser Student.create para que se grabe automáticamente
+      User.new(
+        name: params[:name],
+        lastname: params[:lastname],
+        age: params[:age]
+        ).save
+      redirect_to demos_index_path, notice: 'User submited'
+     else
+      flash[:alert] = 'Please fill the required information'
+      render :index
+     end
+  end
+
   protected
   
   def get_data
